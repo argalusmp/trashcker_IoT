c99c03e27a5f74d45014c90a1985f64b1cd1d968
fahmi <fahmizhd52@gmail.com>
2024-05-26 13:35:22 +0700

    Add table

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import { initFlowbite } from "flowbite";
 
