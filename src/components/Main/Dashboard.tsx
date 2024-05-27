c8e30c54d1462bcfeddc00be0ae56d542a24380a
fahmi <fahmizhd52@gmail.com>
2024-05-27 20:36:18 +0700

    Add modals

--- src/components/Main/Dashboard.tsx
+++ src/components/Main/Dashboard.tsx
@@ -1,7 +1,12 @@
+// import { useState } from 'react';
 import Chart from '../Chart';
 
 export default function Dashboard() {
+  // let [user, setinputvalue1] = useState("");
+  // let [sampah, setinputvalue2] = useState("");
+
   return (
+  <>
   <div className="p-10 sm:ml-64 dark:bg-background-color-theme">
     <div className="dark:bg-outline-color-theme w-full h-14 rounded-xl px-6 text-2xl font-semibold dark:text-white py-3 shadow-2xl">
       Selamat Datang di Bank Sampah, User
@@ -26,91 +31,75 @@ export default function Dashboard() {
         className="w-16 mx-20"
         alt="Wighter Logo"
       />
-      <div className="rounded-2xl bg-secondary-color-theme mx-3 w-46 h-11 flex justify-center items-center font-semibold hover:bg-outline-color-theme cursor-pointer">Timbang</div>
+      <button className="rounded-2xl bg-secondary-color-theme mx-3 w-52 h-11 flex justify-center items-center font-semibold hover:bg-outline-color-theme cursor-pointer" data-modal-target="static-modal" data-modal-toggle="static-modal">Timbang</button>
       </div>
     </div>
     <Chart />
-    {/* <div className="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
-      <div className="flex justify-between pb-4 mb-4 border-b border-gray-200 dark:border-gray-700">
-        <div className="flex items-center">
-          <div className="w-12 h-12 rounded-lg bg-gray-100 dark:bg-gray-700 flex items-center justify-center me-3">
-            <svg className="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 19">
-              <path d="M14.5 0A3.987 3.987 0 0 0 11 2.1a4.977 4.977 0 0 1 3.9 5.858A3.989 3.989 0 0 0 14.5 0ZM9 13h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z"/>
-              <path d="M5 19h10v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2ZM5 7a5.008 5.008 0 0 1 4-4.9 3.988 3.988 0 1 0-3.9 5.859A4.974 4.974 0 0 1 5 7Zm5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm5-1h-.424a5.016 5.016 0 0 1-1.942 2.232A6.007 6.007 0 0 1 17 17h2a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5ZM5.424 9H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h2a6.007 6.007 0 0 1 4.366-5.768A5.016 5.016 0 0 1 5.424 9Z"/>
-            </svg>
-          </div>
-          <div>
-            <h5 className="leading-none text-2xl font-bold text-gray-900 dark:text-white pb-1">3.4k</h5>
-            <p className="text-sm font-normal text-gray-500 dark:text-gray-400">Leads generated per week</p>
-          </div>
-        </div>
-        <div>
-          <span className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-1 rounded-md dark:bg-green-900 dark:text-green-300">
-            <svg className="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
-            </svg>
-            42.5%
-          </span>
-        </div>
-      </div>
 
-      <div className="grid grid-cols-2">
-        <dl className="flex items-center">
-            <dt className="text-gray-500 dark:text-gray-400 text-sm font-normal me-1">Money spent:</dt>
-            <dd className="text-gray-900 text-sm dark:text-white font-semibold">$3,232</dd>
-        </dl>
-        <dl className="flex items-center justify-end">
-            <dt className="text-gray-500 dark:text-gray-400 text-sm font-normal me-1">Conversion rate:</dt>
-            <dd className="text-gray-900 text-sm dark:text-white font-semibold">1.2%</dd>
-        </dl>
-      </div>
-
-      <div id="column-chart"></div>
-        <div className="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
-          <div className="flex justify-between items-center pt-5">
-            <button
-              id="dropdownDefaultButton"
-              data-dropdown-toggle="lastDaysdropdown"
-              data-dropdown-placement="bottom"
-              className="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
-              type="button">
-              Last 7 days
-              <svg className="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-              </svg>
-            </button>
-            <div id="lastDaysdropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Yesterday</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Today</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last 7 days</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last 30 days</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last 90 days</a>
-                  </li>
-                </ul>
+    <div id="static-modal" data-modal-backdrop="static" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div className="relative p-4 w-full max-w-2xl max-h-full">
+        <div className="bg-white relative rounded-lg shadow dark:bg-outline-color-theme">
+            <div className="p-4 md:p-5 space-y-4 flex flex-col">
+              <div className='flex justify-between items-center'>
+                <h3 className='dark:text-white font-semibold'>Tanggal</h3>
+                <div className="flex items-center justify-between rounded-t">
+                    <button type="button" className="text-white bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                    </button>
+                </div>
+              </div>
+                <input className='dark:bg-black dark:text-white rounded-xl' type="date"/>
+              <h3 className='dark:text-white font-semibold'>User</h3>
+                <input className='dark:bg-black dark:text-white rounded-xl' type="text" placeholder='Pilih User'/>
+              <h3 className='dark:text-white font-semibold'>Jenis Sampah</h3>
+                <input className='dark:bg-black dark:text-white rounded-xl' type="text" placeholder='Pilih Jenis Sampah'/>
             </div>
-            <a
-              href="#"
-              className="uppercase text-sm font-semibold inline-flex items-center rounded-lg text-blue-600 hover:text-blue-700 dark:hover:text-blue-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2">
-              Leads Report
-              <svg className="w-2.5 h-2.5 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-              </svg>
-            </a>
-          </div>
+            <div className='flex flex-col justify-center items-center mt-5'>
+              <img
+                src="weighter.png"
+                className="w-32"
+                alt="Wighter Logo"
+              />
+              <h1 className='text-white font-semibold text-3xl'>10 Kg</h1>
+            </div>
+            <div className="flex items-center p-4 md:p-5 rounded-b mt-5">
+                <button data-modal-hide="static-modal" type="button" className="text-black bg-secondary-color-theme hover:bg-gray-200 font-semibold rounded-lg text-sm px-5 py-2.5 w-full text-center" data-modal-target="timbang" data-modal-toggle="timbang">Timbang</button>
+            </div>
         </div>
-    </div> */}
+    </div>
+    </div>
 
+    <div id="timbang" data-modal-backdrop="static" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div className="relative p-4 w-full max-w-2xl max-h-full">
+        <div className="bg-white relative rounded-lg shadow dark:bg-outline-color-theme">
+            <div className="p-4 md:p-5 space-y-4 flex flex-col">
+              <div className='flex justify-between items-center'>
+                <h3 className='dark:text-white font-semibold'>Tanggal</h3>
+                <div className="flex items-center justify-between rounded-t">
+                    <button type="button" className="text-white bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                    </button>
+                </div>
+              </div>
+              <div className='dark:bg-black dark:text-white rounded-xl'></div>
+              <h3 className='dark:text-white font-semibold'>User</h3>
+              <div className='dark:bg-black dark:text-white rounded-xl'></div>
+                {/* <input className='dark:bg-black dark:text-white rounded-xl' type="text" placeholder='Pilih User'/> */}
+              <h3 className='dark:text-white font-semibold'>Jenis Sampah</h3>
+              <div className='dark:bg-black dark:text-white rounded-xl'></div>
+                {/* <input className='dark:bg-black dark:text-white rounded-xl' type="text" placeholder='Pilih Jeni s Sampah'/> */}
+            </div>
+            <div className="flex items-center p-4 md:p-5 rounded-b mt-10">
+                <button data-modal-hide="static-modal" type="button" className="text-black bg-secondary-color-theme hover:bg-gray-200 font-semibold rounded-lg text-sm px-5 py-2.5 w-full text-center" data-modal-target="static-modal" data-modal-toggle="static-modal">Tambang</button>
+            </div>
+        </div>
+    </div>
+    </div>
   </div>
-
+  </>
   );
 }
