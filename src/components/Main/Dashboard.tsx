44347d475de536f6e6181cdfcb0b98d2d889c0e2
Riziq Ramadhan <92849740+Soocotra@users.noreply.github.com>
2024-05-30 01:13:02 +0700

    LGTM!

--- src/components/Main/Dashboard.tsx
+++ src/components/Main/Dashboard.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import Chart from '../Chart';
 import { onAuthStateChange } from "../../services/auth";
-import { User as UserModel, getUsers } from "../../services/user_db";
-import { calculateTotalPrice, calculateTotalWeight } from "../../services/scale_db";
+import { User, User as UserModel, getUsers } from "../../services/user_db";
+import { Scale, addScale, calculateTotalPrice, calculateTotalWeight } from "../../services/scale_db";
 import { Trash as TrashModel, getTrashs } from "../../services/trash_db";
 import { useNavigate } from "react-router-dom";
 import mqtt from "mqtt"
+import { Timestamp } from "firebase/firestore";
 
 function getTodayDate(): string {
   const today = new Date();
@@ -16,7 +17,9 @@ function getTodayDate(): string {
 }
 
 export default function Dashboard() {
-  const [user, setUser] = useState<UserModel | null>(null);
+  const [currentUser, setCurrentUser] = useState<UserModel | null>(null);
+  const [user, setUser] = useState<string>();
+  const [trash, setTrash] = useState<string>();
   const [totalPrice, setTotalPrice] = useState<number>(0);
   const [totalWeight, setTotalWeight] = useState<number>(0);
   const [users, setUsers] = useState<UserModel[]>([]);
@@ -61,7 +64,9 @@ export default function Dashboard() {
   };
 
   useEffect(() => {
-    onAuthStateChange(setUser);
+    onAuthStateChange((user)=>{
+      setCurrentUser(user);
+    });
 
     const fetchData = async () => {
       const price = await calculateTotalPrice();
@@ -118,7 +123,7 @@ export default function Dashboard() {
   <>
   <div id="dashboard" className="p-10 sm:ml-64 dark:bg-background-color-theme">
     <div className="dark:bg-outline-color-theme w-full h-14 rounded-xl px-6 text-2xl font-semibold dark:text-white py-3 shadow-2xl">
-      {`Selamat Datang di Bank Sampah, ${user?.name || 'User'}`}
+      {`Selamat Datang di Bank Sampah, ${currentUser?.name || 'User'}`}
     </div>
     <div className="flex justify-around space-x-10 mb-10 w-692 items-center">
       <div className="h-52 dark:bg-outline-color-theme rounded-xl mt-12 shadow-2xl">
@@ -161,13 +166,13 @@ export default function Dashboard() {
               </div>
               <input className='dark:bg-black dark:text-white rounded-xl' type="date" value={todayDate} />
               <h3 className='dark:text-white font-semibold'>User</h3>
-              <select className='dark:bg-black dark:text-white rounded-xl'>
+              <select className='dark:bg-black dark:text-white rounded-xl' onChange={(e)=> setUser(e.target.value)}>
                 {users.map((user) => (
                   <option key={user.id} value={user.id}>{user.name}</option>
                 ))}
               </select>
               <h3 className='dark:text-white font-semibold'>Jenis Sampah</h3>
-              <select className='dark:bg-black dark:text-white rounded-xl'>
+              <select className='dark:bg-black dark:text-white rounded-xl' onChange={(e)=> setTrash(e.target.value)}>
                 {trashes.map((trash) => (
                   <option key={trash.id} value={trash.id}>{trash.name}</option>
                 ))}
@@ -180,42 +185,33 @@ export default function Dashboard() {
                 alt="Wighter Logo"
               />
               {/* <input type="text" value={ payload.berat??0 }/> */}
-              <h1 className='text-white font-semibold text-3xl'>{ payload.berat??0 }</h1>
+              <h1 className='text-white font-semibold text-3xl'>{ payload.weight ?? 0 }</h1>
             </div>
             <div className="flex items-center p-4 md:p-5 rounded-b mt-5">
-                <button type="button" className="text-black bg-secondary-color-theme hover:bg-gray-200 font-semibold rounded-lg text-sm px-5 py-2.5 w-full text-center" data-modal-target="dashboard">Timbang</button>
+                <button type="button" onClick={onSubmit} className="text-black bg-secondary-color-theme hover:bg-gray-200 font-semibold rounded-lg text-sm px-5 py-2.5 w-full text-center" data-modal-target="dashboard">Simpan</button>
             </div>
         </form>
     </div>
     </div>
-
-    {/* <div id="timbang" data-modal-backdrop="static" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div className="relative p-4 w-full max-w-2xl max-h-full">
-        <div className="bg-white relative rounded-lg shadow dark:bg-outline-color-theme">
-            <div className="p-4 md:p-5 space-y-4 flex flex-col">
-              <div className='flex justify-between items-center'>
-                <h3 className='dark:text-white font-semibold'>Tanggal</h3>
-                <div className="flex items-center justify-between rounded-t">
-                    <button type="button" className="text-white bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
-                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                        </svg>
-                    </button>
-                </div>
-              </div>
-              <div className='dark:bg-black dark:text-white rounded-xl'></div>
-              <h3 className='dark:text-white font-semibold'>User</h3>
-              <div className='dark:bg-black dark:text-white rounded-xl'></div>
-              <h3 className='dark:text-white font-semibold'>Jenis Sampah</h3>
-              <div className='dark:bg-black dark:text-white rounded-xl'></div>
-            </div>
-            <div className="flex items-center p-4 md:p-5 rounded-b mt-10">
-                <button data-modal-hide="static-modal" type="button" className="text-black bg-secondary-color-theme hover:bg-gray-200 font-semibold rounded-lg text-sm px-5 py-2.5 w-full text-center" data-modal-target="static-modal" data-modal-toggle="static-modal">Tambang</button>
-            </div>
-        </div>
-    </div>
-    </div> */}
   </div>
   </>
   );
+
+  async function onSubmit(){
+    if(payload.weight != null && user !=null && trash != null){
+      try {
+        const scale = new Scale(
+          payload?.weight,
+          users.find((e)=> e.id == user),
+          trashes.find((e)=> e.id == trash),
+          trashes.find((e)=> e.id == trash).price * payload?.weight,
+          Timestamp.now(), Timestamp.now() 
+        );
+        await addScale(scale);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 }
+
